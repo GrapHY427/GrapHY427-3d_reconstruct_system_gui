4e8a4bac6a94f72e8795ed9978df8dede8d2d765
GrapHY427 <1170240102@qq.com>
2024-05-14 17:30:37 +0800

    commit 2024.5.14.17.30
    finish joystick control
    finish ListBox Packaging

--- display_lib.py
+++ display_lib.py
@@ -163,7 +163,7 @@ class PackedText:
 
 class ListBox:
     def __init__(self, left_top: tuple, width_height: tuple, ordinary_color: tuple, selected_color: tuple,
-                 text_size: int, text_color: int, text_horizon_offset: int, candidate_list: list,
+                 text_size: int, text_color: tuple, text_horizon_offset: int, candidate_list: list,
                  listbox_hint: str, no_candidate_hint: str):
         self.left_top = left_top
         self.left, self.top = left_top
@@ -171,6 +171,8 @@ class ListBox:
         self.width, self.height = width_height
         self.ordinary_color = ordinary_color
         self.selected_color = selected_color
+        self.mouse_on_color = ordinary_color
+        self.mouse_down_color = ordinary_color
         self.text_size = text_size
         self.text_height = 0.62 * text_size
         self.text_color = text_color
@@ -178,6 +180,8 @@ class ListBox:
         self.text_vertical_offset = (self.width_height[1] - self.text_height) / 2
         self.candidate_list = candidate_list
         self.selected_index = -1
+        self.mouse_on_state = [False for _ in range(len(self.candidate_list))]
+        self.mouse_down_state = [False for _ in range(len(self.candidate_list))]
         self.previous_selected_index = -1
         self.listbox_hint = listbox_hint
         self.no_candidate_text = no_candidate_hint
@@ -189,21 +193,68 @@ class ListBox:
                                       for i in range(len(self.candidate_list))]
         else:
             self.listbox_rect_list = None
+
         # 回调函数
         self.mouse_motion_callback = None
         self.mouse_button_down_callback = None
         self.mouse_button_up_callback = None
 
+    def mouse_motion_service(self, input_event: pygame.event.Event):
+        if self.listbox_rect_list is not None:
+            for i, listbox_item in enumerate(self.listbox_rect_list):
+                if listbox_item.collidepoint(input_event.pos):
+                    self.mouse_on_state[i] = True
+                    self.mouse_down_state[i] = False
+                    self.mouse_motion_callback(self)
+                else:
+                    self.mouse_on_state[i] = False
+
+    def mouse_button_down_service(self, input_event: pygame.event.Event):
+        if self.listbox_rect_list is not None:
+            for i, listbox_item in enumerate(self.listbox_rect_list):
+                if listbox_item.collidepoint(input_event.pos):
+                    self.mouse_down_state[i] = True
+                    self.mouse_on_state[i] = False
+                    self.mouse_button_down_callback(self)
+                else:
+                    self.mouse_down_state[i] = False
+
+    def mouse_button_up_service(self, input_event: pygame.event.Event):
+        if self.listbox_rect_list is not None:
+            for i, listbox_item in enumerate(self.listbox_rect_list):
+                if listbox_item.collidepoint(input_event.pos):
+                    self.mouse_down_state[i] = False
+                    if self.selected_index == i:
+                        self.selected_index = -1
+                    else:
+                        self.selected_index = i
+                    self.mouse_button_up_callback(self)
+
     def set_selected_index(self, selected_index: int):
         self.previous_selected_index = self.selected_index
         self.selected_index = selected_index
+
+    def set_mouse_on_color(self, color: tuple):
+        self.mouse_on_color = color
+
+    def set_mouse_down_color(self, color: tuple):
+        self.mouse_down_color = color
 
     def cancel_selected_index(self):
         self.selected_index = -1
 
     def update_candidate_list(self, candidate_list: list):
+        if len(candidate_list) > len(self.candidate_list):
+            self.mouse_on_state.extend(False for _ in range(len(candidate_list) - len(self.candidate_list)))
+            self.mouse_down_state.extend(False for _ in range(len(candidate_list) - len(self.candidate_list)))
+        elif len(candidate_list) < len(self.candidate_list):
+            self.mouse_on_state = self.mouse_on_state[:len(candidate_list)]
+            self.mouse_down_state = self.mouse_down_state[:len(candidate_list)]
+        else:
+            pass
         self.candidate_list = candidate_list
 
+        # 重绘矩形实例
         self.display_rect = pygame.Rect(self.left_top, self.width_height)
         if len(self.candidate_list) > 0:
             self.listbox_rect_list = [pygame.Rect((self.left, self.top + self.height * (i + 1)), self.width_height)
@@ -213,11 +264,11 @@ class ListBox:
 
     def process_event(self, input_event: pygame.event.Event):
         if input_event.type == pygame.MOUSEMOTION:
-            self.mouse_motion_callback(self, input_event)
+            self.mouse_motion_service(input_event)
         elif input_event.type == pygame.MOUSEBUTTONDOWN:
-            self.mouse_button_down_callback(self, input_event)
+            self.mouse_button_down_service(input_event)
         elif input_event.type == pygame.MOUSEBUTTONUP:
-            self.mouse_button_up_callback(self, input_event)
+            self.mouse_button_up_service(input_event)
 
     def render(self, input_screen: pygame.surface.Surface):
         text_font = pygame.font.Font(None, self.text_size)
@@ -236,14 +287,17 @@ class ListBox:
             input_screen.blit(text, (self.left + self.text_horizon_offset, self.top + self.text_vertical_offset))
 
             # 绘制listbox
-            for i, listbox_item in enumerate(self.listbox_rect_list):
-                pygame.draw.rect(input_screen, self.ordinary_color, listbox_item)
+            pygame.draw.rect(input_screen, self.ordinary_color, self.display_rect)
 
             for i, listbox_item in enumerate(self.listbox_rect_list):
-                pygame.draw.rect(input_screen, self.ordinary_color, listbox_item)
-                if i == self.selected_index:
-                    # 高亮选中选项
+                if self.mouse_on_state[i]:
+                    pygame.draw.rect(input_screen, self.mouse_on_color, listbox_item)
+                elif self.mouse_down_state[i]:
+                    pygame.draw.rect(input_screen, self.mouse_down_color, listbox_item)
+                elif i == self.selected_index:
                     pygame.draw.rect(input_screen, self.selected_color, listbox_item)
+                else:
+                    pygame.draw.rect(input_screen, self.ordinary_color, listbox_item)
 
             text = text_font.render(self.listbox_hint, True, self.text_color)
             input_screen.blit(text, (self.left + self.text_horizon_offset, self.top + self.text_vertical_offset))
--- main.py
+++ main.py
@@ -266,9 +266,8 @@ def render_joystick_control_window(input_screen: pygame.surface.Surface, backgro
     global selected_index
     global is_selecting_com_port
 
-    listbox = None
-    transform_matrix = None
     previous_button_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    clear_window_flag = False
 
     # 注册标题
     joystick_control_text = display_lib.PackedText('Joystick Control', 60, (255, 255, 255), (450, 40))
@@ -353,6 +352,16 @@ def render_joystick_control_window(input_screen: pygame.surface.Surface, backgro
         button.mouse_on_color = (0, 255, 255)
         button.mouse_down_color = (0, 0, 255)
 
+    listbox = display_lib.ListBox(left_top=(635, 200), width_height=(260, 30), ordinary_color=(128, 128, 128),
+                                  selected_color=(0, 255, 0), text_size=32, text_color=(255, 255, 255),
+                                  text_horizon_offset=0, candidate_list=[], listbox_hint='Available Serial Device:',
+                                  no_candidate_hint='No Serial Device!')
+    listbox.set_mouse_down_color((0, 0, 255))
+    listbox.set_mouse_on_color((0, 255, 255))
+    listbox.mouse_button_up_callback = listbox_mouse_up_event_service
+    listbox.mouse_button_down_callback = listbox_mouse_down_event_service
+    listbox.mouse_motion_callback = listbox_mouse_on_event_service
+
     # 运行循环
     while True:
         for event in pygame.event.get():
@@ -360,37 +369,23 @@ def render_joystick_control_window(input_screen: pygame.surface.Surface, backgro
                 current_state_code = state_code['main_menu']
             for button in button_list:
                 button.event_service(event)
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if is_selecting_com_port:
-                    # 检查鼠标是否在某个listbox上
-                    if listbox is not None:
-                        for i, listbox_item in enumerate(listbox):
-                            if listbox_item.collidepoint(event.pos):
-                                if selected_index != i:
-                                    selected_index = i
-                                else:
-                                    selected_index = -1
-                                    if serial_port is not None:
-                                        serial_port.close()
-                                        serial_port = None
-                                        selected_port = None
-                                        control_handle = None
+            listbox.process_event(event)
 
         # 填充背景
         input_screen.blit(background, (0, 0))
 
         # 初始化串口
         if selected_port is not None and serial_port is None:
-            serial_port = serial.Serial(selected_port.name, baud_rate, timeout=time_out)
+            serial_port = serial.Serial(selected_port, baud_rate, timeout=time_out)
             control_handle = communicate_lib.ControlHandle()
 
         if selected_port is not None and serial_port is not None:
-            if serial_port.name != selected_port.name:
-                serial_port = serial.Serial(selected_port.name, baud_rate)
+            if serial_port.name != selected_port:
+                serial_port = serial.Serial(selected_port, baud_rate)
 
         if control_handle is not None:
             # 绘制状态指示文字
-            serial_port_work_status_text.set_text(selected_port.name + ' work now')
+            serial_port_work_status_text.set_text(selected_port + ' work now')
             serial_port_work_status_text.render(input_screen)
 
             num_frames_count_text.set_text(f'Num frames: {num_frames}')
@@ -406,12 +401,10 @@ def render_joystick_control_window(input_screen: pygame.surface.Surface, backgro
 
             transform_matrix_text.set_text('Transform Matrix')
             display_lib.draw_matrix(input_screen, transform_matrix, (630, 330), (70, 35))
-        else:
-            transform_matrix_text.set_text(' ')
-            serial_port_work_status_text.set_text(' ')
 
             state, photo = camera.read()
             cv2.imshow('camera', photo)
+            clear_window_flag = True
 
             # # 发送控制数据到MCU
             if (joystick.get_button(0) == 0) and (previous_button_state[0] == 1):
@@ -427,59 +420,29 @@ def render_joystick_control_window(input_screen: pygame.surface.Surface, backgro
             elif (joystick.get_button(3) == 0) and (previous_button_state[3] == 1):
                 communicate_lib.send_play_music_command(serial_port)
 
+            control_handle.get_joystick_signal(joystick)
+            control_handle.send_speed_control_command(serial_port)
+            control_handle.read_speed_control_report(serial_port, 0)
             for index in range(len(previous_button_state)):
                 previous_button_state[index] = joystick.get_button(index)
 
-            if control_handle is not None:
-                control_handle.get_joystick_signal(joystick)
+        else:
+            if clear_window_flag:
+                cv2.destroyWindow('camera')
+                clear_window_flag = False
 
-                control_handle.send_speed_control_command(serial_port)
-
-                control_handle.read_speed_control_report(serial_port, 0)
+            transform_matrix_text.set_text(' ')
+            serial_port_work_status_text.set_text(' ')
 
         # 串口选择界面
         if is_selecting_com_port:
+            candidate_list = []
             ports_list = list(serial.tools.list_ports.comports())
 
-            # 设置listbox颜色
-            listbox_color = (128, 128, 128)
+            for port in ports_list:
+                candidate_list.append(port.name)
 
-            text_font = pygame.font.Font(None, 32)
-            default_listbox = [pygame.Rect(650, 200, 250, 30)]
-
-            if len(ports_list) <= 0:
-                # 绘制listbox
-                for i, listbox_item in enumerate(default_listbox):
-                    pygame.draw.rect(input_screen, listbox_color, listbox_item)
-
-                text = text_font.render("No serial device!", True, (255, 255, 255))
-                input_screen.blit(text, (650, 210))
-            else:
-                # 创建一个表示listbox的列表
-                listbox = [pygame.Rect(650, 230 + 30 * i, 250, 30) for i in range(len(ports_list))]
-
-                # 绘制listbox
-                for i, listbox_item in enumerate(default_listbox):
-                    pygame.draw.rect(input_screen, listbox_color, listbox_item)
-
-                for i, listbox_item in enumerate(listbox):
-                    pygame.draw.rect(input_screen, listbox_color, listbox_item)
-                    if i == selected_index:
-                        # 高亮选中串口
-                        pygame.draw.rect(input_screen, (0, 0, 250), listbox_item)
-
-                text = text_font.render("Available serial device:", True, (255, 255, 255))
-                input_screen.blit(text, (650, 210))
-
-                for i, comport in enumerate(ports_list):
-                    text = text_font.render(comport.device, True, (255, 255, 255))
-                    input_screen.blit(text, (650, 240 + 30 * i))
-                    if i == selected_index:
-                        selected_port = comport
-
-                        pygame.draw.rect(input_screen, listbox_color, default_listbox[0])
-                        text = text_font.render(comport.device, True, (255, 255, 255))
-                        input_screen.blit(text, (650, 210))
+            listbox.update_candidate_list(candidate_list)
 
         # 读取手柄摇杆值
         for i, axis_text in enumerate(axis_text_list):
@@ -498,6 +461,8 @@ def render_joystick_control_window(input_screen: pygame.surface.Surface, backgro
             text.render(input_screen)
         for packed_text in axis_text_list:
             packed_text.render(input_screen)
+        if is_selecting_com_port:
+            listbox.render(input_screen)
 
         # 刷新屏幕
         pygame.display.flip()
@@ -512,7 +477,227 @@ def render_joystick_control_window(input_screen: pygame.surface.Surface, backgro
 
 # 自动控制窗口, state码: 3
 def render_auto_control_window(input_screen: pygame.surface.Surface, background: pygame.surface.Surface):
-    pass
+    global current_state_code
+
+    global joystick
+    global control_handle
+
+    global frames
+    global num_frames
+
+    # 串口相关实例
+    global selected_port
+    global serial_port
+    global selected_index
+    global is_selecting_com_port
+
+    previous_button_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    clear_window_flag = False
+
+    # 注册标题
+    joystick_control_text = display_lib.PackedText('Automated Control', 60, (255, 255, 255), (450, 40))
+    joystick_monitor_text = display_lib.PackedText('Joystick Monitor', 36, (255, 255, 255), (60, 120))
+    motor_info_text = display_lib.PackedText('Motor Info', 36, (255, 255, 255), (320, 120))
+
+    # 读取并显示每个轴的状态
+    axis_text_list = []
+    # # # 读取并显示每个轴的状态
+    num_axes = joystick.get_numaxes()
+    for i in range(num_axes):
+        axis_text_list.append(display_lib.PackedText(f'Axis {i}: 0', 36, (0, 255, 255), (100, i * 50 + 200)))
+
+    # 方便修改窗口位置相关参数
+    width = 300
+    height = 200
+    step = 30
+    text_color = (0, 255, 0)
+    button_color = (255, 255, 0)
+
+    # 注册按钮
+    quit_button = display_lib.Button(left_top=(490, 590), width_height=(200, 80), color=button_color, text='Main Menu',
+                                     text_size=42, text_color=(0, 0, 0), text_horizon_offset=25, border_radius=10)
+    select_com_button = display_lib.Button(left_top=(900, 200), width_height=(180, 40), color=button_color,
+                                           text='Select COM Port', text_size=28, text_color=(0, 0, 0),
+                                           text_horizon_offset=10, border_radius=10)
+    y_axis_zero_button = display_lib.Button(left_top=(940, 300), width_height=(180, 40), color=button_color,
+                                            text='Zero Y Axis', text_size=28, text_color=(0, 0, 0),
+                                            text_horizon_offset=30, border_radius=10)
+    z_axis_zero_button = display_lib.Button(left_top=(940, 350), width_height=(180, 40), color=button_color,
+                                            text='Zero Z Axis', text_size=28, text_color=(0, 0, 0),
+                                            text_horizon_offset=30, border_radius=10)
+    pitch_zero_button = display_lib.Button(left_top=(940, 400), width_height=(180, 40), color=button_color,
+                                           text='Zero Pitch', text_size=28, text_color=(0, 0, 0),
+                                           text_horizon_offset=30, border_radius=10)
+    reset_frame_button = display_lib.Button(left_top=(940, 500), width_height=(180, 40), color=button_color,
+                                            text='Reset Frame', text_size=28, text_color=(0, 0, 0),
+                                            text_horizon_offset=30, border_radius=10)
+    save_frame_button = display_lib.Button(left_top=(940, 550), width_height=(180, 40), color=button_color,
+                                           text='Save Frame', text_size=28, text_color=(0, 0, 0),
+                                           text_horizon_offset=30, border_radius=10)
+
+    # 注册提示用文字
+    serial_port_work_status_text = display_lib.PackedText(' ', 36, (255, 255, 255), (650, 500))
+    num_frames_count_text = display_lib.PackedText(' ', 28, (255, 255, 255), (950, 610))
+    transform_matrix_text = display_lib.PackedText(' ', 28, (128, 0, 255), (650, 300))
+
+    # 创建文字列表
+    text_list = [joystick_control_text, joystick_monitor_text, motor_info_text, serial_port_work_status_text,
+                 num_frames_count_text, transform_matrix_text]
+
+    text_list[0] = joystick_control_text
+    text_list[1] = joystick_monitor_text
+    text_list[2] = motor_info_text
+    text_list[3] = serial_port_work_status_text
+    text_list[4] = num_frames_count_text
+    text_list[5] = transform_matrix_text
+
+    # 创建按钮列表
+    button_list = [quit_button, select_com_button, y_axis_zero_button, z_axis_zero_button, pitch_zero_button,
+                   reset_frame_button, save_frame_button]
+    button_list[0] = quit_button
+    button_list[1] = select_com_button
+    button_list[2] = y_axis_zero_button
+    button_list[3] = z_axis_zero_button
+    button_list[4] = pitch_zero_button
+    button_list[5] = reset_frame_button
+    button_list[6] = save_frame_button
+
+    # 注册回调函数
+    quit_button.mouse_button_up_callback = main_menu_button_mouse_up_event_handler
+    select_com_button.mouse_button_up_callback = select_com_button_mouse_button_up_event_handler
+    y_axis_zero_button.mouse_button_up_callback = y_axis_zero_button_mouse_button_up_event_handler
+    z_axis_zero_button.mouse_button_up_callback = z_axis_zero_button_mouse_button_up_event_handler
+    pitch_zero_button.mouse_button_up_callback = pitch_zero_button_mouse_button_up_event_handler
+    reset_frame_button.mouse_button_up_callback = reset_frame_button_mouse_button_up_event_handler
+    save_frame_button.mouse_button_up_callback = save_frame_button_mouse_button_up_event_handler
+
+    for button in button_list:
+        button.mouse_motion_callback = mouse_on_event_handler
+        button.mouse_button_down_callback = mouse_down_event_handler
+        button.mouse_on_color = (0, 255, 255)
+        button.mouse_down_color = (0, 0, 255)
+
+    listbox = display_lib.ListBox(left_top=(635, 200), width_height=(260, 30), ordinary_color=(128, 128, 128),
+                                  selected_color=(0, 255, 0), text_size=32, text_color=(255, 255, 255),
+                                  text_horizon_offset=0, candidate_list=[], listbox_hint='Available Serial Device:',
+                                  no_candidate_hint='No Serial Device!')
+    listbox.set_mouse_down_color((0, 0, 255))
+    listbox.set_mouse_on_color((0, 255, 255))
+    listbox.mouse_button_up_callback = listbox_mouse_up_event_service
+    listbox.mouse_button_down_callback = listbox_mouse_down_event_service
+    listbox.mouse_motion_callback = listbox_mouse_on_event_service
+
+    # 运行循环
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygloc.QUIT:
+                current_state_code = state_code['main_menu']
+            for button in button_list:
+                button.event_service(event)
+            listbox.process_event(event)
+
+        # 填充背景
+        input_screen.blit(background, (0, 0))
+
+        # 初始化串口
+        if selected_port is not None and serial_port is None:
+            serial_port = serial.Serial(selected_port, baud_rate, timeout=time_out)
+            control_handle = communicate_lib.ControlHandle()
+
+        if selected_port is not None and serial_port is not None:
+            if serial_port.name != selected_port:
+                serial_port = serial.Serial(selected_port, baud_rate)
+
+        if control_handle is not None:
+            # 绘制状态指示文字
+            serial_port_work_status_text.set_text(selected_port + ' work now')
+            serial_port_work_status_text.render(input_screen)
+
+            num_frames_count_text.set_text(f'Num frames: {num_frames}')
+            serial_port_work_status_text.render(input_screen)
+
+            transform_matrix_text.render(input_screen)
+
+            transform_matrix = matrix_lib.compute_transform_matrix(
+                [0,
+                 ((220 - control_handle.z_axis_position) / 220),
+                 (510 - control_handle.y_axis_position) / 510],
+                [0, control_handle.pitch_angle, control_handle.yaw_angle / 22.76])
+
+            transform_matrix_text.set_text('Transform Matrix')
+            display_lib.draw_matrix(input_screen, transform_matrix, (630, 330), (70, 35))
+
+            state, photo = camera.read()
+            cv2.imshow('camera', photo)
+            clear_window_flag = True
+
+            # # 发送控制数据到MCU
+            if (joystick.get_button(0) == 0) and (previous_button_state[0] == 1):
+                if control_handle is not None:
+                    io_lib.add_transform_matrix_to_frame(
+                        dataset_dir + str(num_frames) + '.png', 0, transform_matrix, frames)
+                    io_lib.capture_photo_to_dataset(photo_dir, num_frames, photo)
+                    if state:
+                        num_frames += 1
+                    else:
+                        io_lib.remove_last_transform_matrix_to_frame(frames)
+
+            elif (joystick.get_button(3) == 0) and (previous_button_state[3] == 1):
+                communicate_lib.send_play_music_command(serial_port)
+
+            control_handle.get_joystick_signal(joystick)
+            control_handle.send_speed_control_command(serial_port)
+            control_handle.read_speed_control_report(serial_port, 0)
+            for index in range(len(previous_button_state)):
+                previous_button_state[index] = joystick.get_button(index)
+
+        else:
+            if clear_window_flag:
+                cv2.destroyWindow('camera')
+                clear_window_flag = False
+
+            transform_matrix_text.set_text(' ')
+            serial_port_work_status_text.set_text(' ')
+
+        # 串口选择界面
+        if is_selecting_com_port:
+            candidate_list = []
+            ports_list = list(serial.tools.list_ports.comports())
+
+            for port in ports_list:
+                candidate_list.append(port.name)
+
+            listbox.update_candidate_list(candidate_list)
+
+        # 读取手柄摇杆值
+        for i, axis_text in enumerate(axis_text_list):
+            axis = joystick.get_axis(i)
+            if i < 4:
+                axis = int(axis * 64)
+            else:
+                axis = int((axis + 1) * 128)
+            axis_text.set_text(f'Axis {i}: {axis:d}')
+
+        # 渲染控件
+        display_lib.draw_motor_info(input_screen, 36, text_color, (width, height), step, control_handle)
+        for button in button_list:
+            button.render(input_screen)
+        for text in text_list:
+            text.render(input_screen)
+        for packed_text in axis_text_list:
+            packed_text.render(input_screen)
+        if is_selecting_com_port:
+            listbox.render(input_screen)
+
+        # 刷新屏幕
+        pygame.display.flip()
+
+        # 控制更新速率
+        pygame.time.Clock().tick(60)
+
+        # 如果状态码不在手柄控制上，则退出循环
+        if current_state_code != state_code['auto_control_window']:
+            return
 
 
 def main_loop():
@@ -680,6 +865,32 @@ def save_frame_button_mouse_button_up_event_handler(input_button: display_lib.Bu
         io_lib.save_json_file(json_name, camera_angle_x, frames)
         frames = []
         num_frames = 0
+
+
+def listbox_mouse_up_event_service(input_listbox: display_lib.ListBox):
+    global serial_port
+    global selected_port
+    global control_handle
+    global is_selecting_com_port
+
+    if input_listbox.selected_index == -1:
+        if serial_port is not None:
+            serial_port.close()
+            serial_port = None
+            selected_port = None
+            control_handle = None
+    else:
+        selected_port = input_listbox.candidate_list[input_listbox.selected_index]
+
+
+def listbox_mouse_down_event_service(input_listbox: display_lib.ListBox):
+    if input_listbox.selected_index == -1:
+        pass
+
+
+def listbox_mouse_on_event_service(input_listbox: display_lib.ListBox):
+    if input_listbox.selected_index == -1:
+        pass
 
 
 if __name__ == '__main__':
